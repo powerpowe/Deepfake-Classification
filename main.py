a66b8da086ce39fd34dd11ff7c1a5748b7fa7945
powerpowe <2001lbj49@gmail.com>
2023-06-11 21:28:17 +0900

    Final Project

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import torch.cuda
 
 from module import utils, training_testing
 from dataset import data
+from realworld_data import testing_realworld
 
 setting = {"model_name": "xception",  # xception, efficientnet_b0
-           "quality": 'low',  # high, low
+           "quality": 'high',  # high, low
            'dataset_name': 'nt'}  # f2f, nt
 
 hyperparameter = {'batch_size': 32,
                   'lr': 1e-4,
-                  'epoch': 100}
+                  'epoch': 15}
 
 if __name__ == "__main__":
     model = utils.download_model(setting["model_name"], 2, True)
@@ -21,6 +22,9 @@ if __name__ == "__main__":
 
     model = training_testing.training(model, train_dset, valid_dset, device, True, hyperparameter)
 
-    test_dset = data.DataSet(setting['quality'], setting['dataset_name'], 'test')
-    training_testing.testing(model, test_dset, device)
+    # model.to(device)
+    test_dset = testing_realworld.RealWorldDataSet()
+
+    testing_realworld.testing(model, test_dset, device)
+
 
